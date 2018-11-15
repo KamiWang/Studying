94baab177e844583e001f1bf00c17eb6102ec184
WangMoujie <wmj0619@qq.com>
2018-11-15 21:33:21 +0800

    autocommit

--- Python/sanic/xmath/math_base.py
+++ Python/sanic/xmath/math_base.py
@@ -1,27 +1,37 @@
 import math
 
+
 digit_map = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
              'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
-def _radix_convert(number, src_base, dst_base, reverse=False):
-    result_str = None
+def _radix_interger_convert(number, src_base, dst_base):
     base_10_num = int(number, src_base)
-
     if dst_base == 10:
-        result_str = str(base_10_num)
-        return result_str[::-1] if reverse else result_str
-
+        return str(base_10_num)
     temp = list()
-
     while base_10_num:
         temp.append(digit_map[base_10_num % dst_base])
         base_10_num //= dst_base
-
     temp.reverse()
     result_str = "".join(temp)
+    return result_str
 
-    return result_str[::-1] if reverse else result_str
+
+def _radix_decimals_convert(number, src_base, dst_base):
+    base_10_num = 0.0
+    for i in range(len(number)):
+        x = int(number[i], 36)
+        base_10_num += x * (src_base**-(i+1))
+
+    if dst_base == 10:
+        return str(base_10_num).split('.')[1]
+
+    return base_10_num
+
+
+s = _radix_decimals_convert("0", 2, 10)
+print(s)
 
 
 def radix_convert(src, src_base: int = 0, dst_base: int = 10):
@@ -36,9 +46,9 @@ def radix_convert(src, src_base: int = 0, dst_base: int = 10):
             src_base = 10
 
     src_str = str(src).split(".")
-    result = _radix_convert(src_str[0], src_base, dst_base)
+    result = _radix_interger_convert(src_str[0], src_base, dst_base)
 
     if len(src_str) > 1:
-        result += '.' + _radix_convert(src_str[1], src_base, dst_base, True)
+        result += '.' + _radix_interger_convert(src_str[1], src_base, dst_base)
 
     return result
