b50670c6a583a17aea1d81e1568b12f9b8a40cf2
WangMoujie <wmj0619@qq.com>
2018-11-16 11:25:14 +0800

    autocommit

--- Python/sanic/xmath/math_base.py
+++ Python/sanic/xmath/math_base.py
@@ -14,24 +14,33 @@ def _radix_interger_convert(number, src_base, dst_base):
         temp.append(digit_map[base_10_num % dst_base])
         base_10_num //= dst_base
     temp.reverse()
-    result_str = "".join(temp)
-    return result_str
+    if not temp:
+        temp.append('0')
+
+    return "".join(temp)
 
 
 def _radix_decimals_convert(number, src_base, dst_base):
     base_10_num = 0.0
-    for i in range(len(number)):
-        x = int(number[i], 36)
-        base_10_num += x * (src_base**-(i+1))
+    if 10 != src_base:
+        for i in range(len(number)):
+            x = int(number[i], 36)
+            base_10_num += x * (src_base**-(i+1))
+    else:
+        base_10_num = float("0." + number)
 
     if dst_base == 10:
         return str(base_10_num).split('.')[1]
 
-    return base_10_num
+    temp = list()
+    for _ in range(100):
+        if 0 == base_10_num:
+            break
+        base_10_num *= dst_base
+        temp.append(digit_map[int(str(base_10_num).split('.')[0])])
+        base_10_num = math.modf(base_10_num)[0]
 
-
-s = _radix_decimals_convert("0", 2, 10)
-print(s)
+    return "".join(temp)
 
 
 def radix_convert(src, src_base: int = 0, dst_base: int = 10):
@@ -45,10 +54,15 @@ def radix_convert(src, src_base: int = 0, dst_base: int = 10):
         else:
             src_base = 10
 
-    src_str = str(src).split(".")
-    result = _radix_interger_convert(src_str[0], src_base, dst_base)
+    if src_base < 2 or src_base > 36 or dst_base < 2 or dst_base > 36:
+        raise ValueError("base must be >= 2 and <= 36")
 
-    if len(src_str) > 1:
-        result += '.' + _radix_decimals_convert(src_str[1], src_base, dst_base)
+    src_parts = str(src).split(".")
+    result = _radix_interger_convert(src_parts[0], src_base, dst_base)
+
+    if len(src_parts) > 1:
+        decimals_str = _radix_decimals_convert(src_parts[1], src_base, dst_base)
+        if decimals_str:
+            result += '.' + decimals_str
 
     return result
