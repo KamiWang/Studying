6cf1c8754a45b126bc946376eba2eaa7f0ca56d9
WangMoujie <wmj0619@qq.com>
2018-11-27 20:28:00 +0800

    autocommit

---
+++ Python/kami/crawler/http_request.py
@@ -0,0 +1,49 @@
+import aiohttp
+import asyncio
+
+
+default_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36",
+                   "Accept-Language": "zh-CN,zh;q=0.9", "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
+
+
+class HTTPRequestClient:
+    def __init__(self):
+        self.session = None
+        self.headers = default_headers
+
+    async def __aenter__(self):
+        await self.init()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.destory()
+
+    async def init(self):
+        self.session = aiohttp.ClientSession()
+
+    async def destory(self):
+        await self.session.close()
+
+    async def get(self, url):
+        async with self.session.get(url, headers=self.headers) as response:
+            return await response.text(self.get_coding(response))
+
+    def get_coding(self, response):
+        charset = "utf-8"
+        try:
+            charset = response.get_encoding()
+        except Exception:
+            return charset
+        if charset.find("gb") >= 0:
+            charset = "gb18030"
+        return charset
+
+
+async def go():
+    async with HTTPRequestClient() as client:
+        html = await client.get("https://www.qq.com")
+        print(html)
+
+
+if __name__ == "__main__":
+    asyncio.run(go())
--- Python/kami/mysql/mysql_pool.py
+++ Python/kami/mysql/mysql_pool.py
@@ -11,12 +11,19 @@ class MySQLConntionPool:
         self.db = db
         self.pool = None
 
-    def __del__(self):
-        if self.pool:
-            self.pool.close()
+    async def __aenter__(self):
+        await self.init()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.destory()
 
     async def init(self):
         self.pool = await aiomysql.create_pool(host=self.host, port=self.port, user=self.user, password=self.password, db=self.db)
+
+    async def destory(self):
+        self.pool.close()
+        await self.pool.wait_closed()
 
     async def query(self, sql):
         async with self.pool.get() as conn:
@@ -43,12 +50,11 @@ class MySQLConntionPool:
 
 
 async def go():
-    mysql = MySQLConntionPool(host="192.168.1.214", port=3306, user='root', password='0hOpPCWhvLfX00xe', db='wmj_logiclog')
-    result = await mysql.query("select * from ypds_log_model_1 where id =-1")
-    print(result)
-    # async for x in mysql.query_row("select * from ypds_log_model_1"):
-    #     print(x)
-
+    async with MySQLConntionPool(host="192.168.1.214", port=3306, user='root', password='0hOpPCWhvLfX00xe', db='wb_test_log') as pool:
+        result = await pool.query("select * from ypds_log_model_1")
+        print(result)
+        async for x in pool.query_row("select * from ypds_log_model_1"):
+            print(x)
 
 if "__main__" == __name__:
     asyncio.run(go())
